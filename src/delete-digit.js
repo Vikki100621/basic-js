cf53b293aae92e1597fa2840855ae32baeebf5f9
Viktoriya <kruckovaviktoria421@gmail.com>
2023-04-18 00:56:37 +0300

    fix: update some function

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  n = String(n).split('');
+
+  let maxnum = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < n.length; i++) {
+    let num = Number(n.slice(0, i).join('') + n.slice(i + 1).join(''));
+    maxnum = Math.max(maxnum, num);
+  }
+  return maxnum;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str); 
+
+  let repeatTimes = options.repeatTimes === undefined  ? 1 : Number(options.repeatTimes);
+  let separator = options.separator === undefined ? '+' : options.separator;
+  let addition = options.addition === undefined ? '' : String(options.addition);
+  let additionRepeatTimes = options.additionRepeatTimes === undefined ? 1 : Number(options.additionRepeatTimes);
+  let additionSeparator = options.additionSeparator === undefined ? '|' : options.additionSeparator;
+
+  let repeatedAddition = addition;
+  if (additionRepeatTimes > 1) {
+    repeatedAddition = (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+  }
+
+  let repeatedStr = str + repeatedAddition;
+  if (repeatTimes > 1) {
+    repeatedStr = (str + repeatedAddition + separator).repeat(repeatTimes - 1) + str + repeatedAddition;
+  }
+
+  return repeatedStr;
 }
 
 module.exports = {
